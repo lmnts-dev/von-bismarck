24ad1c4632ed017a7ecef54194adcb475e6a91ed
Jose Rios <jose@commence.studio>
2023-12-04 13:09:31 -0800

    :construction: More file

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -73,7 +73,7 @@ const Menu = props => (
 
         { (data.allDataJson.nodes[0].menus.menu_2_label && data.allDataJson.nodes[0].menus.menu_2_file) &&
           <DownloadLink
-            href={data.allDataJson.nodes[0].menus.menu_2_file}
+            href='/11-17-23%20VB%20-%20Menu%202020-64%20copy.pdf'
             download
           >
             <span>{data.allDataJson.nodes[0].menus.menu_2_label}</span>
