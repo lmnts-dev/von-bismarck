bd0ea147c08aa8f4e28cbb05fdda674b2de90bd5
Jose Rios <jose@commence.studio>
2023-12-04 12:58:12 -0800

    :rocket: Undo

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -73,7 +73,7 @@ const Menu = props => (
 
         { (data.allDataJson.nodes[0].menus.menu_2_label && data.allDataJson.nodes[0].menus.menu_2_file) &&
           <DownloadLink
-            href='../../../11-17-23 VB - Menu 2020-64 copy.pdf'
+            href={data.allDataJson.nodes[0].menus.menu_2_file}
             download
           >
             <span>{data.allDataJson.nodes[0].menus.menu_2_label}</span>
