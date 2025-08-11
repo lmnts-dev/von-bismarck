388bfd9f2b693af50a4d5a35c60981a54d1c58d2
Jose Rios <jlrios.ro@gmail.com>
2025-08-10 21:15:31 -0400

    remove styles

--- src/pages/index.js
+++ src/pages/index.js
@@ -88,7 +88,7 @@ const IndexPage = () => (
         >
           <Col>
             <Ribbon /> 
-            <p style="margin-top: 12px;">Reserve by Sept 16th</p>
+            <p>Reserve by Sept 16th</p>
             <AbsolutePageBtn
               label="Reserve Oktoberfest Table"
               to="https://von-bismarck.square.site/"
@@ -137,7 +137,7 @@ const IndexPage = () => (
           <Col>
             <ContactStrip />
             <Ribbon />
-            <p style="margin-bottom: 12px;">Reserve by Sept 16th</p>
+            <p>Reserve by Sept 16th</p>
             <Row justifyContent="center">
               <AbsolutePageBtn
                 label="Reserve a Table"
